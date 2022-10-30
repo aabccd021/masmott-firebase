2201db30bd61ecc92bef677e14201f58de29af64
aabccd021 <aabccd021@gmail.com>
2022-10-30 17:13:38 +0700

    refactor: better refactor

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,45 +9,35 @@ import { beforeEach } from 'vitest';
 
 import { mkStack } from '../src';
 
-// demo project config for test
-const projectId = 'demo';
-const storageBucket = 'demo.appspot.com';
-const firebaseConfig = {
-  projectId,
-  storageBucket,
+const config = {
+  projectId: 'demo',
+  storageBucket: 'demo.appspot.com',
 
   // https://stackoverflow.com/a/72839398/9611638
   apiKey: 'demoapikey',
 };
-
-// emulator config
 const firestoreHost = 'localhost';
 const firestorePort = 8080;
 
-// init client
 // https://firebase.google.com/docs/emulator-suite/connect_and_prototype#connect_your_app_to_the_emulators
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
 connectAuthEmulator(getAuth(app), 'http://localhost:9099');
 connectStorageEmulator(getStorage(app), 'localhost', 9199);
 connectFirestoreEmulator(getFirestore(app), firestoreHost, firestorePort);
 
-// init admin
 // https://firebase.google.com/docs/emulator-suite/connect_storage#admin_sdks
-admin.initializeApp({ projectId });
+admin.initializeApp({ projectId: config.projectId });
 
 beforeEach(async () => {
   // clear storage
-  const [files] = await admin.storage().bucket(storageBucket).getFiles();
+  const [files] = await admin.storage().bucket(config.storageBucket).getFiles();
   await Promise.all(files.map((file) => file.delete()));
 
-  // clear firestore
   // https://firebase.google.com/docs/emulator-suite/connect_firestore#clear_your_database_between_tests
   await fetch(
-    `http://${firestoreHost}:${firestorePort}/emulator/v1/projects/${projectId}/databases/(default)/documents`,
-    {
-      method: 'DELETE',
-    }
+    `http://${firestoreHost}:${firestorePort}/emulator/v1/projects/${config.projectId}/databases/(default)/documents`,
+    { method: 'DELETE' }
   );
 });
 
-independencyTests(mkStack(firebaseConfig));
+independencyTests(mkStack(config));
