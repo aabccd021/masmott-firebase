849110aa2b48b9f4205ad5f04afcf28cea536e97
aabccd021 <aabccd021@gmail.com>
2022-10-30 13:52:09 +0700

    feat: add firestore test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import * as FIRESTORE from 'firebase/firestore/lite';
 import {
   connectStorageEmulator,
   getDownloadURL,
@@ -7,8 +8,9 @@ import {
   ref,
   uploadString,
 } from 'firebase/storage';
-import { either as E, taskEither as TE } from 'fp-ts';
-import { flow } from 'fp-ts/function';
+import { either as E, io, task as T, taskEither as TE } from 'fp-ts';
+import { flow, pipe } from 'fp-ts/function';
+import * as std from 'fp-ts-std';
 import * as _fs from 'fs/promises';
 import * as Masmott from 'masmott';
 import { MkStack, Stack } from 'masmott';
@@ -26,12 +28,18 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 // eslint-disable-next-line functional/no-expression-statement
 connectAuthEmulator(auth, 'http://localhost:9099');
+
 export const storage = getStorage(app);
 // eslint-disable-next-line functional/no-expression-statement
 connectStorageEmulator(storage, 'localhost', 9199);
+
+export const db = FIRESTORE.getFirestore(app);
+// eslint-disable-next-line functional/no-expression-statement
+FIRESTORE.connectFirestoreEmulator(db, 'localhost', 8080);
 
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -51,8 +59,28 @@ service firebase.storage {
 }
 `;
 
-export const deploy: Stack['admin']['deploy'] = (c) =>
-  fs.writeFile('storage.rules', getStorageRule(c.storage?.securityRule?.type === 'allowAll'));
+const getFirestoreRule = (allow: boolean) => `
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if ${allow};
+    }
+  }
+}
+`;
+
+export const deployStorage: Stack['admin']['deploy']['storage'] = (c) =>
+  pipe(
+    fs.writeFile('storage.rules', getStorageRule(c.securityRule?.type === 'allowAll')),
+    T.chainFirst(() => std.task.sleep(std.date.mkMilliseconds(250)))
+  );
+
+export const deployDb: Stack['admin']['deploy']['db'] = (c) =>
+  pipe(
+    fs.writeFile('firestore.rules', getFirestoreRule(c.securityRule?.type === 'allowAll')),
+    T.chainFirst(() => std.task.sleep(std.date.mkMilliseconds(250)))
+  );
 
 export const storageDir = 'masmott';
 
@@ -71,21 +99,53 @@ const getDownloadUrl: Stack['client']['storage']['getDownloadUrl'] = ({ key }) =
         (knownError) =>
           match(knownError)
             .with({ code: 'storage/object-not-found' }, (_) =>
-              Masmott.GetDownloadUrlError.Union.of.NotFound({})
+              Masmott.GetDownloadUrlError.Union.of.FileNotFound({})
             )
             .exhaustive()
       )
     )
   );
 
+const setDoc: Stack['client']['db']['setDoc'] =
+  ({ key, data }) =>
+  () =>
+    FIRESTORE.setDoc(FIRESTORE.doc(db, key.collection, key.id), data);
+
+const getDataFromSnapshot =
+  (snapshot: FIRESTORE.DocumentSnapshot): io.IO<FIRESTORE.DocumentData | undefined> =>
+  () =>
+    snapshot.data();
+
+const getDoc: Stack['client']['db']['getDoc'] = ({ key }) =>
+  pipe(
+    TE.tryCatch(
+      () => FIRESTORE.getDoc(FIRESTORE.doc(db, key.collection, key.id)),
+      (unknownErr) => Masmott.GetDocError.Union.of.Unknown({ value: unknownErr })
+    ),
+    TE.chain(
+      flow(
+        getDataFromSnapshot,
+        io.map(E.fromNullable(Masmott.GetDocError.Union.of.DocNotFound({}))),
+        TE.fromIOEither
+      )
+    )
+  );
+
 export const mkStack: MkStack = async () => ({
   admin: {
-    deploy,
+    deploy: {
+      db: deployDb,
+      storage: deployStorage,
+    },
   },
   client: {
     storage: {
       upload,
       getDownloadUrl,
+    },
+    db: {
+      setDoc,
+      getDoc,
     },
   },
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,20 +1,32 @@
 import { deleteObject, listAll, ref } from 'firebase/storage';
-import { readonlyArray, task as T } from 'fp-ts';
-import { pipe } from 'fp-ts/function';
-import { runTests } from 'masmott';
+import { readonlyArray, task as T, taskEither as TE } from 'fp-ts';
+import { identity, pipe } from 'fp-ts/function';
+import { independencyTests } from 'masmott';
+import fetch from 'node-fetch';
 import { beforeEach } from 'vitest';
 
-import { deployStorageRule, mkStack, storage, storageDir } from '../src';
+import { deployDb, deployStorage, mkStack, storage, storageDir } from '../src';
 
 const beforeEachTask = pipe(
   T.Do,
-  T.chainFirst(() => deployStorageRule({ storage: { securityRule: { type: 'allowAll' } } })),
-  T.bind('files', () => () => listAll(ref(storage, storageDir))),
-  T.chainFirst(({ files }) =>
+  T.chain(() => deployStorage({ securityRule: { type: 'allowAll' } })),
+  T.chain(() => () => listAll(ref(storage, storageDir))),
+  T.chain((files) =>
     pipe(
       files.items,
       readonlyArray.fromArray,
       readonlyArray.traverse(T.ApplicativePar)((delRef) => () => deleteObject(delRef))
+    )
+  ),
+  T.chain(() => deployStorage({ securityRule: {} })),
+  T.chain(() => deployDb({ securityRule: {} })),
+  T.chain(() =>
+    TE.tryCatch(
+      () =>
+        fetch('http://localhost:8080/emulator/v1/projects/demo/databases/(default)/documents', {
+          method: 'DELETE',
+        }),
+      identity
     )
   )
 );
@@ -23,4 +35,4 @@ beforeEach(async () => {
   await beforeEachTask();
 });
 
-runTests(mkStack);
+independencyTests(mkStack);
