4456b3bd4c3f36f793cd7f683661c16eef378198
aabccd021 <aabccd021@gmail.com>
2022-11-27 14:48:35 +0700

    test(red): independent auth

--- src/client/auth/createUserAndSignInWIthEmailAndPassword.ts
+++ src/client/auth/createUserAndSignInWIthEmailAndPassword.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { taskEither } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
+import * as Masmott from 'masmott';
 
 import type { Client } from '../../type';
 
@@ -9,9 +11,13 @@ type Type = Client['auth']['createUserAndSignInWithEmailAndPassword'];
 export const createUserAndSignInWithEmailAndPassword: Type =
   (env) =>
   ({ email, password }) =>
-    pipe(
-      env.firebaseConfig,
-      initializeApp,
-      getAuth,
-      (auth) => () => createUserWithEmailAndPassword(auth, email, password)
+    pipe(env.firebaseConfig, initializeApp, getAuth, (auth) =>
+      taskEither.tryCatch(
+        async () => {
+          // eslint-disable-next-line functional/no-expression-statement
+          await createUserWithEmailAndPassword(auth, email, password);
+        },
+        (value) =>
+          Masmott.CreateUserAndSignInWithEmailAndPasswordError.Union.of.ProviderError({ value })
+      )
     );
--- src/client/auth/signOut.ts
+++ src/client/auth/signOut.ts
@@ -1,10 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signOut as _signOut } from 'firebase/auth';
+import { taskEither } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
+import * as Masmott from 'masmott';
 
 import type { Client } from '../../type';
 
 type Type = Client['auth']['signOut'];
 
 export const signOut: Type = (env) =>
-  pipe(env.firebaseConfig, initializeApp, getAuth, (auth) => () => _signOut(auth));
+  pipe(env.firebaseConfig, initializeApp, getAuth, (auth) =>
+    taskEither.tryCatch(
+      () => _signOut(auth),
+      (value) => Masmott.SignOutError.Union.of.ProviderError({ value })
+    )
+  );
