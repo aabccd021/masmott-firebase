4218534b37b09383a05c8693b1207918686e380e
aabccd021 <aabccd021@gmail.com>
2022-12-06 05:13:30 +0700

    test: 43/45

--- src/ci/deployDb.ts
+++ src/ci/deployDb.ts
@@ -1,5 +1,6 @@
-import { option, readonlyArray, readonlyRecord, readonlyTuple, taskEither } from 'fp-ts';
+import { console, option, readonlyArray, readonlyRecord, readonlyTuple, taskEither } from 'fp-ts';
 import { flow, pipe } from 'fp-ts/function';
+import type { Option } from 'fp-ts/Option';
 import * as std from 'fp-ts-std';
 import * as fs from 'fs/promises';
 import type { Stack as StackT } from 'masmott';
@@ -14,35 +15,52 @@ const getRuleStr = (rule: StackT.ci.DeployDb.True | undefined) =>
     option.map((content) => `\n      allow get: if ${content};`)
   );
 
-const equalRuleStr = (_collectionName: string, comparable: StackT.ci.DeployDb.Comparable[0]) =>
+const equalRuleStr = (comparable: StackT.ci.DeployDb.Comparable[0]) =>
   match(comparable)
     .with({ type: 'AuthUid' }, () => 'request.auth.uid')
     .with({ type: 'DocumentField' }, ({ fieldName }) => `request.resource.data.${fieldName}`)
     .exhaustive();
 
-const createRuleStr = (
-  collectionName: string,
-  nullableRule: StackT.ci.DeployDb.CreateRule | undefined
-) =>
+const createSecurityRuleStr = (nullableRule: StackT.ci.DeployDb.CreateRule | undefined) =>
   pipe(
     option.fromNullable(nullableRule),
-
     option.map((rule) =>
       match(rule)
         .with({ type: 'True' }, () => 'true')
         .with(
           { type: 'Equal' },
-          ({ compare: [lhs, rhs] }) =>
-            `${equalRuleStr(collectionName, lhs)} == ${equalRuleStr(collectionName, rhs)}`
+          ({ compare: [lhs, rhs] }) => `${equalRuleStr(lhs)} == ${equalRuleStr(rhs)}`
         )
         .exhaustive()
-    ),
+    )
+  );
 
-    option.map((content) => `\n     allow create: if ${content};`)
+const createSchemaRuleStr = (schema: StackT.ci.DeployDb.Schema): Option<string> =>
+  pipe(
+    schema,
+    readonlyRecord.fromRecord,
+    readonlyRecord.mapWithIndex((fieldName, field) =>
+      match(field)
+        .with({ type: 'IntField' }, () => `request.resource.data.${fieldName} is int`)
+        .with({ type: 'StringField' }, () => `request.resource.data.${fieldName} is string`)
+        .exhaustive()
+    ),
+    readonlyRecord.toReadonlyArray,
+    readonlyArray.map(readonlyTuple.snd),
+    option.fromPredicate(readonlyArray.isNonEmpty),
+    option.map(std.readonlyArray.join('\n     && '))
+  );
+
+const createRuleStr = (conf: StackT.ci.DeployDb.CollectionConfig) =>
+  pipe(
+    [createSecurityRuleStr(conf.securityRule?.create), createSchemaRuleStr(conf.schema)],
+    readonlyArray.sequence(option.Applicative),
+    option.map(std.readonlyArray.join('\n     && ')),
+    option.map((content) => `\n      allow create: if ${content};`)
   );
 
 const collectionRuleStr = (collectionName: string) => (content: string) =>
-  `\n    match /${collectionName}/{documentId} {\n${content}\n      } `;
+  `\n    match /${collectionName}/{documentId} {\n${content}\n    } `;
 
 const allRuleStr = (content: string) => `
 rules_version = '2';
@@ -56,10 +74,7 @@ const getFirestoreRuleStr = (rules: StackT.ci.DeployDb.Param): string =>
     rules,
     readonlyRecord.mapWithIndex((collectionName, collectionRule) =>
       pipe(
-        [
-          getRuleStr(collectionRule.securityRule?.get),
-          createRuleStr(collectionName, collectionRule.securityRule?.create),
-        ],
+        [getRuleStr(collectionRule.securityRule?.get), createRuleStr(collectionRule)],
         readonlyArray.compact,
         option.fromPredicate(readonlyArray.isNonEmpty),
         option.map(flow(std.readonlyArray.join('\n'), collectionRuleStr(collectionName)))
@@ -70,7 +85,7 @@ const getFirestoreRuleStr = (rules: StackT.ci.DeployDb.Param): string =>
     readonlyArray.compact,
     option.fromPredicate(readonlyArray.isNonEmpty),
     option.map(std.readonlyArray.join('\n')),
-    option.map((content) => `  match /databases/{database}/documents {\n${content}\n}`),
+    option.map((content) => `  match /databases/{database}/documents {\n${content}\n  }`),
     option.getOrElseW(
       () => '  match /{document=**} { \n      allow read, write: if false; \n    }'
     ),
@@ -83,6 +98,7 @@ export const deployDb: Stack['ci']['deployDb'] = () => (rules) =>
       () => fs.writeFile('firestore.rules', getFirestoreRuleStr(rules)),
       (value) => ({ code: 'ProviderError', value })
     ),
+    taskEither.chainIOK(() => console.log(getFirestoreRuleStr(rules))),
     taskEither.chainTaskK(() => std.task.sleep(std.date.mkMilliseconds(250))),
     taskEither.fromTask
   );
