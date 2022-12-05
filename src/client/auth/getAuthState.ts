c79043b40a4687e28cf1acce32f2ed8fbb793db7
aabccd021 <aabccd021@gmail.com>
2022-12-05 19:06:17 +0700

    fix: augmentCapability

--- src/client/auth/getAuthState.ts
+++ src/client/auth/getAuthState.ts
@@ -1,22 +1,9 @@
-import { ioRef, option, task, taskEither } from 'fp-ts';
-import { pipe } from 'fp-ts/function';
-import type { AuthState } from 'masmott';
+import { augmentCapability } from 'masmott';
 
 import type { Stack } from '../../type';
 import { onAuthStateChanged } from './onAuthStateChanged';
 
 type Type = Stack['client']['auth']['getAuthState'];
 
-export const getAuthState: Type = (env) =>
-  pipe(
-    ioRef.newIORef<AuthState>(option.none),
-    task.fromIO,
-    task.chain((authStateRef) =>
-      pipe(
-        task.fromIO(onAuthStateChanged(env)(authStateRef.write)),
-        task.chain((unsubscribe) => task.fromIO(unsubscribe)),
-        task.chain(() => task.fromIO(authStateRef.read))
-      )
-    ),
-    taskEither.fromTask
-  );
+export const getAuthState: Type =
+  augmentCapability.getAuthState.fromOnAuthStateChanged(onAuthStateChanged);
