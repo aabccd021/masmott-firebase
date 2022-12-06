e7fdfb666edb67ac413e9a93ce3f16fe6fcc874b
aabccd021 <aabccd021@gmail.com>
2022-12-06 08:01:34 +0700

    fix: compile pass

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,21 @@
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import { apply, reader } from 'fp-ts';
+import { makeFunctions, stack } from 'masmott-firebase';
+
+const readerS = apply.sequenceS(reader.Apply);
+
+export const masmottFunctions = makeFunctions({
+  functions: { path: '', exportName: '' },
+  server: readerS({
+    db: readerS(stack.server.db),
+  })({
+    firebaseAdminApp: admin.initializeApp({ projectId: 'demo' }),
+  }),
+});
+
+import * as functions from 'firebase-functions';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
 });
