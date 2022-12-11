116184c878e0026edf382e85100e16c50cf5c999
aabccd021 <aabccd021@gmail.com>
2022-12-11 13:44:52 +0700

    Serial retry

--- test/serial.test.ts
+++ test/serial.test.ts
@@ -2,8 +2,4 @@ import { independence } from 'masmott/dist/cjs/test';
 
 import { runSuite } from './util';
 
-independence.allSuites
-  .map((suite) =>
-    suite.name === 'functions is independent between test' ? { ...suite, retry: 3 } : suite
-  )
-  .forEach((suite) => runSuite({ suite }));
+independence.allSuites.forEach((suite) => runSuite({ suite }));
